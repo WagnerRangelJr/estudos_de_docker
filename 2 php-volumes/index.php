4ce3d0465991169840ba8b1769bf8ac1143f262f
Wagner Rangel <wrangeljr@gmail.com>
2024-05-15 20:05:17 -0300

    Conteudo dos estudos de docker, container, imagem, networks, volumes, compose

---
+++ 2 php-volumes/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagens</title>
+</head>
+<body>
+   <h1>Escreva sua mensagem! Bind mount é vida</h1>
+   <form action="process.php" method="post">
+    <input type="text" name="message" id="message">
+
+    <input type="submit" value="Enviar Mensagens" id="id">
+   </form> 
+</body>
+</html>
